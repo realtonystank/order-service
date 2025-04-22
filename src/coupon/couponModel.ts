5dc194b3227bb601d7defbfa0d4f1d06b96d23a4
Priyansh Rajwar <rajwars.priyansh@gmail.com>
2025-04-22 21:04:07 +0530

    create coupon schema

---
+++ src/coupon/couponModel.ts
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const couponSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  code: {
+    type: String,
+    required: true,
+  },
+  discount: {
+    type: Number,
+    required: true,
+  },
+  validUpto: {
+    type: Date,
+    required: true,
+  },
+  tenantId: {
+    type: Number,
+    required: true,
+  },
+});
+
+export default mongoose.model("Coupon", couponSchema);
